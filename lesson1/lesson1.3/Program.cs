ac205ef26cab63077a2a00aeedb95c39eb671bf0
istchanar <istchanar@gmail.com>
2021-02-23 23:33:53 +0700

    lesson1_fix

--- lesson1/lesson1.3/Program.cs
+++ lesson1/lesson1.3/Program.cs
@@ -8,23 +8,65 @@ namespace lesson1._3
         {
             Console.WriteLine("Проверка числа Фибоначи");
 
+            int[] array = {-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16}; //Скармливаем массив чисел;
 
-            uint FibCicl = FibonacciCicl(uint number);
+            
+        // Если на вход попадут минусы, отлавливаем их, т.к. будут лишние нули.
 
-            uint FibReq = FibonacciReq(uint number);
+            for (int i = 0; i < array.Length; i++)
+            {
+                int number = array[i];
+
+                if(number<0)
+                { 
+                    int FibReq = FibonacciCicl(number);
+
+                    Console.WriteLine($"Число Фибоначчи циклом {FibReq}");
+                }
+                else
+                {
+                    Console.WriteLine($"Число меньше единицы");
+                }
+            }
+                
+            // Если пойдет число с минусом в рекурсию, получим переполнение, поэтому используем try
+
+            for (int i = 0; i < array.Length; i++)
+                {
+
+                uint number;
+
+                try
+                {
+                    number = Convert.ToUInt32(array[i]);
+
+                    uint FibReq = FibonacciReq(number);
+
+                    Console.WriteLine($"Число Фибоначчи рекурсией {FibReq}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+
+            }
         }
 
 
-        static uint FibonacciCicl(uint number)
+        static int FibonacciCicl(int number)
         {
-            uint a = 0;
-            uint b = 1;
-            uint start;
+            int a = 0;
 
-            for (uint i = 0; i < number; i++)
+            int b = 1;
+
+            int start;
+
+            for (int i = 0; i < number; i++)
             {
                 start = a;
+
                 a = b;
+
                 b += start;
             }
 
@@ -33,18 +75,14 @@ namespace lesson1._3
 
         static uint FibonacciReq(uint number)
         {
+            uint numberUint = Convert.ToUInt32(number);
 
-            if (number == 0 || number == 1)
+            if (numberUint == 0 || numberUint == 1)
             {
-                return number;
+                return numberUint;
             }
 
-            if (number == 1)
-            {
-                return 1;
-            }
-
-            return FibonacciReq(number - 1) + FibonacciReq(number - 2);
+            return FibonacciReq(numberUint - 1) + FibonacciReq(numberUint - 2);
 
         }
     }
