f0f9277f9dcc01bb33872de17dd8ee1b8ea4a99b
istchanar <istchanar@gmail.com>
2021-03-09 21:08:19 +0700

    lesson4

---
+++ lesson4/BinaryTree4.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinaryTree4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
