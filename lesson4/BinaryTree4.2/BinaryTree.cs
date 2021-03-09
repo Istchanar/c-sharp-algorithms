018fcce1aa839c0d06e0dea0297af25feb5dd51f
istchanar <istchanar@gmail.com>
2021-03-09 22:55:01 +0700

    lesson4_start

---
+++ lesson4/BinaryTree4.2/BinaryTree.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree4._2
+{
+    public class BinaryTree : ITree
+    {
+        public TreeNode RootNode { get; set; }
+
+        public TreeNode RigthNode { get; set; }
+
+        public TreeNode LeftNode { get; set; }
+
+        public TreeNode GetRoot()
+        {
+            if (RootNode == null)
+                return null;
+            else if (RootNode != null)
+            { 
+            
+            }
+
+        }
+
+        public void AddItem(int value) // добавить узел
+        {
+            if (RootNode == null)
+            {
+                RootNode = new TreeNode { Value = value };
+            }
+            else if (RootNode.LeftChild == null)
+            {
+                LeftNode = new TreeNode { Value = value };
+
+                LeftNode.Parent = RootNode;
+
+                RootNode.LeftChild = LeftNode;
+            }
+            else if (RootNode.RightChild == null)
+            {
+                RigthNode = new TreeNode { Value = value };
+
+                RigthNode.Parent = RootNode;
+
+                RootNode.RightChild = RigthNode;
+            }
+        }
+
+        public void RemoveItem(int value) // удалить узел по значению
+        {
+
+        }
+
+        public TreeNode GetNodeByValue(int value) //получить узел дерева по значению
+        {
+           var x = TreeNode x;
+
+            return x;
+        }
+        public void PrintTree() //вывести дерево в консоль
+        { 
+        
+        }
+    }
+}
---
+++ lesson4/BinaryTree4.2/NodeInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTree4._2
+{
+    public class NodeInfo
+    {
+            public int Depth { get; set; }
+            public TreeNode Node { get; set; }
+
+    }
+}
