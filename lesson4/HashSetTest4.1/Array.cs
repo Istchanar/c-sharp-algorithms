b90eaacefdd8523e5497c08132ab080199a0aa0d
istchanar <istchanar@gmail.com>
2021-03-11 20:57:03 +0700

    start 4.1

---
+++ lesson4/HashSetTest4.1/Array.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashSetTest4._1
+{
+    public class Array
+    {
+        static string[] array { get; set; }
+
+        static Array()
+        {
+            array = new string[10000];
+        }
+        public void AddValue()
+        {
+            array = new string[10000];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                string text = $"string{i}";
+
+                array[i] = text;
+            }
+        }
+
+        
+        public string GetValue(int index)
+        {
+            return array[index];
+        }
+    }
+}
--- lesson4/HashSetTest4.1/Program.cs
+++ lesson4/HashSetTest4.1/Program.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
 
 namespace HashSetTest4._1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+
+            /*
+            var hashSet = new HashSet<User>();
+
+            var user = new User() { FirstName = "Barbara", SecondName = "Santa" };
+
+            hashSet.Add(user);
+
+            var searchUsser = new User() { FirstName = "Barbara", SecondName = "Santa" };
+
+            Console.WriteLine($"contains user {hashSet.Contains(user)}, contains searchUsser {hashSet.Contains(searchUsser)}");
+
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+            */
+        }
+
+
+        public class BechmarkClass
+        {
+            Array testArray = new Array();
+
+            [Benchmark]
+            public void TestVal()
+            {
+                string value = testArray.GetValue(5000);
+            }
         }
     }
 }
+
