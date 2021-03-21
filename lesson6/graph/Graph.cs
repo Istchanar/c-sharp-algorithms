b7dae627de394a5fdee3608d4127dbd9a120f549
istchanar <istchanar@gmail.com>
2021-03-22 02:18:53 +0700

    lesson6_fix5

--- lesson6/graph/Graph.cs
+++ lesson6/graph/Graph.cs
@@ -7,7 +7,7 @@ namespace graph
     class Graff
     {
         public int[,] graphMatrix; // Матрица смежности;
-        public void SearchDeep()
+        public void DFSearch()
         {
             int height1 = 0; 
             int height2 = 0;
@@ -41,10 +41,12 @@ namespace graph
                 }
             }
             Console.WriteLine("DFS список вершин графа:");
+            Console.WriteLine();
             for (int i = 0; i < node.Count; i++)
             {
                 Console.Write(node[i] + " ");
             }
+            Console.WriteLine();
             Console.WriteLine("Глубина графа:" + height2);
         }
         
@@ -52,20 +54,24 @@ namespace graph
         {
             Queue<int> vertex = new Queue<int>();   // Создаём очередь;
             vertex.Enqueue(0);  // Добавляем в очередь граф со значением 0;
-            bool[ ] used = new bool[graphMatrix.GetLength(0)]; //Создаём массив равный одному из измерений массива-графа, для хранения состояния вершины графа;
-            used[0] = true; // Для нуля сразу устанавливаем тру, т.е. мы его посетили;
+            bool[ ] visited = new bool[graphMatrix.GetLength(0)]; //Создаём массив равный одному из измерений массива-графа, для хранения состояния вершины графа;
+            visited[0] = true; // Для нуля сразу устанавливаем тру, т.е. мы его посетили;
             int i = 0;  //Счётчик нумерации волн;
-            Console.WriteLine($"Волна {i}, голова 0");
-            Console.WriteLine();
+            List<int> graphValues = new List<int>();
+            graphValues.Add(0);
+            Console.WriteLine("Вывод волн BFSearch алгоритма:");
+            Console.WriteLine($"Волна {i}");
+            Console.WriteLine("Голова 0");
 
             while (vertex.Count != 0)
             {
                 int vertexNode = vertex.Dequeue(); //Удаляем вершину графа;
                 for (int index = 0; index < graphMatrix.GetLength(0); index++) //Для нас связи графа - это значения матрицы, где индекс - это значение вершины графа, 
                 {                                                        //а присуствие значение в матрице, по индексу значение говорит о связи с другой вершиной.
-                    if (graphMatrix[index, vertexNode] != 0 && used[index] != true)          // проверка есть ли связь и посещали ли мы вершину;
+                    if (graphMatrix[index, vertexNode] != 0 && visited[index] != true)          // проверка есть ли связь и посещали ли мы вершину;
                     {
-                        used[index] = true;
+                        visited[index] = true;
+                        graphValues.Add(index);
                         vertex.Enqueue(index);
                     }
                 }
@@ -76,9 +82,13 @@ namespace graph
                     Console.Write(vert);
                 }
                 Console.WriteLine();
-                
             }
-            Console.WriteLine(vertex.Count);
+            Console.WriteLine();
+            Console.WriteLine("BFS список вершин графа:");
+            foreach (int graphVal in graphValues)
+            {
+                Console.Write(graphVal+" ");
+            }
         }
     }
 }
--- lesson6/graph/Program.cs
+++ lesson6/graph/Program.cs
@@ -7,15 +7,14 @@ namespace graph
         static void Main(string[] args)
         {                                  //0 1 2 3 4 5
             int[,] graphMatrix = new int[6, 6]   {{0,1,0,0,1,0},
-                                            {1,0,1,0,0,1},
-                                            {0,1,0,1,0,1},
-                                            {0,0,1,0,1,0},
-                                            {1,0,0,1,0,1},
-                                            {0,1,1,0,1,0}};
+                                                  {1,0,1,0,0,1},
+                                                  {0,1,0,1,0,1},
+                                                  {0,0,1,0,1,0},
+                                                  {1,0,0,1,0,1},
+                                                  {0,1,1,0,1,0}};
 
-            Graff graph = new Graff { tree = graphMatrix };
-            graph.SearchDeep();
-            Console.WriteLine("Вывод вол BFSearch алгоритма:");
+            Graff graph = new Graff { graphMatrix = graphMatrix };
+            graph.DFSearch();
             Console.WriteLine();
             graph.BFSearch(graphMatrix);
             Console.ReadLine();
